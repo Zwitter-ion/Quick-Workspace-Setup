22aebfb1b182439d0f3378695d60befca4167972
Parth Sareen <parthsareen2006@gmail.com>
2022-05-07 10:05:44 +0530

    Formatted py files via pycharm

--- Run.py
+++ Run.py
@@ -1,16 +1,16 @@
 """ ---Importing The Modules--- """
-import os #For directory changing
-from Brain import Run_App #For running the app
+import os  # For directory changing
+from Brain import Run_App  # For running the app
 
 """ ---Setting The Path--- """
-path = os.getcwd() + '\Data\Test_infromation.qes' #Path to the data file
+path = os.getcwd() + '\Data\Test_infromation.qes'  # Path to the data file
 
 """ ---Running The Apps--- """
-with open(path, 'r') as data_file: #Opening the data file
-    info = data_file.readlines() #Reading the data file
-    for apps in info: #For each app in the data file
-        apps = apps.split(' | ') #Splitting the path and file into a list
-        app = Run_App() #Creating an instance of the Run_App class
-        app.path = apps[0] #Setting the path
-        app.file = apps[1] #Setting the file
-        app.start_app() #Running the app
+with open(path, 'r') as data_file:  # Opening the data file
+    info = data_file.readlines()  # Reading the data file
+    for apps in info:  # For each app in the data file
+        apps = apps.split(' | ')  # Splitting the path and file into a list
+        app = Run_App()  # Creating an instance of the Run_App class
+        app.path = apps[0]  # Setting the path
+        app.file = apps[1]  # Setting the file
+        app.start_app()  # Running the app
