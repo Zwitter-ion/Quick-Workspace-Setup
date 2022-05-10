c67c8cff51211395b07750603c623d7e07625385
Parth Sareen <66111742+parth25sareen@users.noreply.github.com>
2022-05-10 17:39:24 +0530

    Made changes and finally linked core.py with core.kv completely

--- Core.py
+++ Core.py
@@ -2,34 +2,40 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.button import Button
+from Run import Start_App
+from Add import add_mode
 
 Builder.load_file('Core.kv')
 
 
-class BakeRecipes(Widget):
+class Main(Widget):
     def __init__(self, **kwargs):
-        super(BakeRecipes, self).__init__(**kwargs)
+        super(Main, self).__init__(**kwargs)
 
-        
 
-        with open('Data\\Mode_list.qes', 'r') as modes:
-            self.modes = modes.read().splitlines()
+        try:
 
-            for i in self.modes:  # assuming you need to create 5 buttons
-                self.button = Button(text=str(i))  # the text on the button
-                # self.button.bind(on_press=self.pressed) #when the button is clicked
-                self.ids.grid.add_widget(self.button)  # added to the grid
 
-                #     self.add_widget(Label(text=f"Error: {e}"))
+            with open('Data\\Mode_list.qes', 'r') as modes:
+                self.modes = modes.read().splitlines()
 
-                # self.add = Button(text="Add Mode", background_color=(10, 10, 0, .75), font_size=30, font_name='Comic', color=(200, 200, 200, 1), size_hint= (0.5, 1))
-                # self.add.bind(on_release= lambda x: add_mode())
-                # self.add_widget(self.add)
+                for items in self.modes:  # assuming you need to create 5 buttons
+                    self.button = Button(text=str(
+                        items), background_color=(200, 200,200,1), background_normal = '', color='##2E4053')  # the text on the button
+                    self.button.bind(on_press= lambda x: Start_App().verify_data_files(items)) #when the button is clicked
+                    self.ids.grid.add_widget(self.button)  # added to the grid
 
+                self.add = Button(text='Add Mode', background_color=(
+                    200, 200, 200, 1), background_normal='', color='##2E4053')
+                self.add.bind(on_press= lambda x: add_mode())
+                self.ids.add.add_widget(self.add)
+
+        except Exception as error:
+            print(error)
 
 class Quick_Env_Setup(App):
     def build(self):
-        return BakeRecipes()
+        return Main()
 
 
 if __name__ == '__main__':
